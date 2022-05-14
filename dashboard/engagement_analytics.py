2d84cb2fc725181d84782838b6f3d9ceb0f877ed
dagi <dagmawi.yoha03@gmail.com>
2022-05-15 01:38:31 +0300

    add the header and chart type for email

--- dashboard/engagement_analytics.py
+++ dashboard/engagement_analytics.py
@@ -17,3 +17,8 @@ def application():
     df_youtube = pd.read_csv('data/top10_youtube_users.csv')
     df_session = pd.read_csv('data/top10_user_session.csv')
     df_DLUL = pd.read_csv('data/top10_DLUL_users.csv')
+
+    st.header("Top 10 Users Engaged Per Each Application")
+    st.subheader("Email App")
+    st.dataframe(df_email)
+    st.bar_chart(df_email.Email_Total_Data)
