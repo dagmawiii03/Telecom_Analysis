7174c2106a1d3ab2905d8000c991629f5a6ba295
dagi <dagmawi.yoha03@gmail.com>
2022-05-15 01:23:01 +0300

    added appropriate csvs to a df

--- dashboard/experience_analytics.py
+++ dashboard/experience_analytics.py
@@ -1,4 +1,15 @@
 import streamlit as st
 import pandas as pd
 import sys
-import sys
+import os
+
+def application():
+    st.title ('user_Experience_Analytics')
+    st.header ("Data visualization")
+
+    df_avgthr = pd.read_csv ('data/top10avgThroughput.csv')
+    df_rtt = pd.read_csv('data/top10rtt.csv')
+    df_tcp = pd.read_csv('data/top10tcp.csv')
+    df_frqThr = pd.read_csv('data/most_freqAvgThr.csv')
+    df_frqrtt = pd.read_csv('data/most_freqRTT.csv')
+    df_frqtcp = pd.read_csv('data/most_freqTCP.csv')
