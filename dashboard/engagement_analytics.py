12cfd7ce68f5e1e20ae690349f81336d0af1ea32
dagi <dagmawi.yoha03@gmail.com>
2022-05-15 01:37:02 +0300

    added the title and important csvs to their respective dfs

--- dashboard/engagement_analytics.py
+++ dashboard/engagement_analytics.py
@@ -3,3 +3,17 @@ import pandas as pd
 import os
 import sys
 
+def application():
+
+    st.title("User Engagement Analysis")
+
+    st.header("Data Visualization")
+    df_email = pd.read_csv('data/top10_email_users.csv')
+    df_game = pd.read_csv('data/top10_gameApp_users.csv')
+    df_google = pd.read_csv('data/top10_google_users.csv')
+    df_netflix = pd.read_csv('data/top10_netflix_users.csv')
+    df_otherAct = pd.read_csv('data/top10_otherAct_users.csv')
+    df_social = pd.read_csv('data/top10_socialMedia_users.csv')
+    df_youtube = pd.read_csv('data/top10_youtube_users.csv')
+    df_session = pd.read_csv('data/top10_user_session.csv')
+    df_DLUL = pd.read_csv('data/top10_DLUL_users.csv')
